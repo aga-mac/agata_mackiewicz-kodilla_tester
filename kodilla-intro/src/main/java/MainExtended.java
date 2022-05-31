685d1bbb6129b98346213c5e53f60c58af53b0bf
Agata Mackiewicz <mackiewicz.a9@gmail.com>
2022-05-31 14:06:03 +0200

    Main_Extended

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        int eiffleTowerHeight = 324;
+        String bestFood = "pizza";
+        char favouriteLetter = 'J';
+        boolean dogBestManFriend = (true);
+        String secondName = "Zofia";
+        int booksOnShelf = 20;
+        System.out.println(eiffleTowerHeight);
+        System.out.println(bestFood);
+        System.out.println(favouriteLetter);
+        System.out.println(dogBestManFriend);
+        System.out.println(secondName);
+        System.out.println(booksOnShelf);
+    }
+}
