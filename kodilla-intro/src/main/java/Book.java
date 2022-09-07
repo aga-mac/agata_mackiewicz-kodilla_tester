9d4831405727863eeb79f1a663c27029012196d9
Agata Mackiewicz <mackiewicz.a9@gmail.com>
2022-09-08 00:08:27 +0200

    zadanie 2.7

--- kodilla-intro/src/main/java/Book.java
+++ kodilla-intro/src/main/java/Book.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Book {
+public class Book {
+    private String author;
+    private String book;
+
+    private Book (String author, String book) {
+        this.author = author;
+        this.book = book;
+    }
+
+    public static Book of (String author, String title) {
+        return new Book(author, title);
+    }
+
+    public static void main(String[] args) {
+        Book book = Book.of ("S.Rooney", "Normal people");
+    }
+
 }
+
+
