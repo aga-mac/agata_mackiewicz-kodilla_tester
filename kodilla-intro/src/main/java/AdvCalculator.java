332b3100d4a68760def196740a244130c5e62242
Agata Mackiewicz <mackiewicz.a9@gmail.com>
2022-09-08 00:13:07 +0200

    switch

--- kodilla-intro/src/main/java/AdvCalculator.java
+++ kodilla-intro/src/main/java/AdvCalculator.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class AdvCalculator {
+public class AdvCalculator {
+    public double calculate() {
+        String userSelected = UserDialogs.getUserSelection();
+        int a = UserDialogs.getValue();
+        int b = UserDialogs.getValue();
+        switch (userSelected) {
+            case "ADD": return a + b;
+            case "SUB": return a - b;
+            case "DIV": return a / b;
+            case "MUL": return a * b;
+            default: return 0;
+        }
+    }
 }
+//switch (userSelected) { case "ADD":
+        //result = a + b;
+        //break;                     // [1]
+        //case "SUB":
+        //result = a - b;
+        //break;                     // [2]
+        //case "DIV":
+       // result = a / b;
+        //break;                     // [3]
+        //case "MUL":
+       // result = a * b;
+        //break;                     // [4]
+        //}
+        //return result;
+        //}
