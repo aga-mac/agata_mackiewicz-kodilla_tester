240da8f961cea18e807dcf12e7dc11817ab26ac2
Agata Mackiewicz <mackiewicz.a9@gmail.com>
2022-09-10 13:44:17 +0200

    dział 3.1

--- kodilla-basic-tests/src/main/java/com/kodilla/inheritance/Convertible.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/inheritance/Convertible.java
@@ -1,2 +1,18 @@
-package com.kodilla.inheritance;public class Convertible {
+package com.kodilla.inheritance;
+
+public class Convertible extends Car {
+    public void openRoof () {
+        System.out.println("Opening roof...");
+    }
+    public void closeRoof() {
+        System.out.println("Closing roof...");
+    }
+    @Override
+    public void openDoors() {
+        System.out.println("Opening 2 doors");
+    }
+    public Convertible(int wheels, int seats) {
+        super(wheels, seats);   // instrukacja super zawsze musi być na pierwszym miejscu w konstruktorze
+
+    }
 }
