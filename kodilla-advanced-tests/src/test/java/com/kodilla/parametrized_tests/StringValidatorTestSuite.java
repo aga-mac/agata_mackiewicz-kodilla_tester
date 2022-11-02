5e2a61e7c079821911ac46bfe36d5cb5f35a8e3e
Agata Mackiewicz <mackiewicz.a9@gmail.com>
2022-11-02 08:53:08 +0100

    zadanie 7.1

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringValidatorTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringValidatorTestSuite.java
@@ -1,4 +1,24 @@
+package com.kodilla.parametrized_tests;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class StringValidatorTestSuite {
-  
+    private StringValidator validator = new StringValidator();
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "  "})
+    public void shouldReturnTrueIfStringIsEmpty(String text) {
+        assertTrue(validator.isBlank(text));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    public void shouldReturnTrueIfStringIsEmpty1(String text) {
+        assertTrue(validator.isBlank(text));
+    }
+
 }
--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/UserValidatorTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/UserValidatorTestSuite.java
@@ -1,4 +1,49 @@
+package com.kodilla.parametrized_tests.homework;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class UserValidatorTestSuite {
-  
+
+    UserValidator userValidator = new UserValidator();
+
+    @ParameterizedTest
+    @EmptySource
+    public void shouldReturnFalseIfUsernameIsEmpty(String username) {
+        assertFalse(userValidator.validateUsername(username));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"AgATAa", "1390", "A_G-", "-._.-"})
+    public void shouldReturnTrueIfUsernameIsCorrect(String username) {
+        assertTrue(userValidator.validateUsername(username));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"ma", "euhr+_.", "nick92&", "Ag"})
+    public void shouldReturnFalseIfUsernameIsWrong(String username) {
+        assertFalse(userValidator.validateUsername(username));
+    }
+
+    @ParameterizedTest
+    @NullSource
+    public void shouldReturnFalseIfUsermailIsNullOrEmpty(String email) {
+        assertFalse(userValidator.validateEmail(email));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"aga_mac@gmail.com", "kasia.nowak@wp.pl", "A_9mack@gmail.com", "Gb5_h@interia.pl"})
+    public void shouldReturnTrueIfUsermailIsCorrect(String email) {
+        assertTrue(userValidator.validateEmail(email));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"Aga*Mac.Kie$@wp.pl", "asdf.gmail.com.pl", "kasia.nowak@@interia.pl", ",#macki$_@yahoo.com"})
+    public void shouldReturnFalseIfUsermailIsWrong(String email) {
+        assertFalse(userValidator.validateEmail(email));
+    }
 }
